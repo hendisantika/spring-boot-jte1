de39996f2bcae82ddca705c4cd322610f9d72faf
hendisantika <hendisantika@yahoo.co.id>
2025-04-07 08:31:18 +0700

    feat: Add TemplateController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jte1/controller/TemplateController.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.jte1.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jte1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 07/04/25
+ * Time: 08.31
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class TemplateController {
+}
